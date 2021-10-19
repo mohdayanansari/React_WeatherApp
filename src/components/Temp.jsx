e641e706d9a0bbcc7508c0c3499a074f1e3fc11e
Ayan Ansari <1100zaid@gmail.com>
2021-10-19 11:10:18 +0530

    UI Changed

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -28,9 +28,7 @@ const Temp = () => {
         country,
         sunset,
       };
-      
 
-     
       setTempInfo(myNewWeatherInfo);
     } catch (error) {
       console.log(error);
@@ -43,7 +41,38 @@ const Temp = () => {
 
   return (
     <>
-      <div className="wrap">
+      <div className="h-screen w-screen p-10 relative">
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-red-700 text-2xl sm:text-4xl lg:text-6xl pb-10 uppercase font-bold text-center my-font">
+          Search for weather
+        </h1>
+        <div className="flex justify-center w-full gap-2 mb-16">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            className="w-8/12 sm:w-5/12 lg:w-5/12 rounded-lg text-white text-opacity-70 bg-color outline-none border-none text-2xl shadow-inner"
+          />
+          <button
+            className="bg-gradient-to-l px-5 lg:w-2/12 rounded-lg text-xl text-white bg-blue-400 from-blue-700 "
+            onClick={getWeatherInfo}
+            type="button"
+          >
+            Search
+          </button>
+        </div>
+
+        <WeatherCard {...tempInfo} />
+
+
+        <p className="text-white text-center text-xs lg:mt-2 mt-5 text-opacity-50">Made by <a href="https://github.com/objectorienteddev07">Ayan Ansari</a> || 2021</p>
+      </div>
+
+      {/* <div className="wrap">
         <input
           type="search"
           placeholder="search..."
@@ -61,7 +90,7 @@ const Temp = () => {
         </button>
       </div>
 
-      <WeatherCard tempInfo={tempInfo}  />
+      <WeatherCard {...tempInfo} /> */}
     </>
   );
 };
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
 
-const WeatherCard = ({ tempInfo }) => {
+const WeatherCard = ({
+  temp,
+  humidity,
+  pressure,
+  weathermood,
+  name,
+  speed,
+  country,
+  sunset,
+}) => {
   const [weatherState, setWeatherState] = useState();
-//   console.log(tempInfo.weathermood);
-//   console.log(tempInfo);
+
+  //   console.log(tempInfo.weathermood);
+  //   console.log(tempInfo);
   useEffect(() => {
-    if (tempInfo.weathermood) {
-      switch (tempInfo.weathermood) {
+    if (weathermood) {
+      switch (weathermood) {
         case "Clouds":
           setWeatherState("wi-day-cloudy");
           break;
@@ -16,7 +26,7 @@ const WeatherCard = ({ tempInfo }) => {
         case "Clear":
           setWeatherState("wi-day-sunny");
           break;
-        case "Rainy":
+        case "Rain":
           setWeatherState("wi-day-rain");
           break;
         case "Mist":
@@ -28,35 +38,100 @@ const WeatherCard = ({ tempInfo }) => {
           break;
       }
     }
-  }, [tempInfo.weathermood]);
+  }, [weathermood]);
 
-
-
-
-  let sec = tempInfo.sunset;
+  let sec = sunset;
   let date = new Date(sec * 1000);
   let timeStr = `${date.getHours()}:${date.getMinutes()}`;
   return (
     <>
+      <div className="w-full flex flex-col items-center ">
+        <i className={`wi ${weatherState} text-white text-9xl`}></i>
+        <p className="text-blue-500 text-lg font-light pt-8">{weathermood}</p>
+        <p className="text-white text-xl text-opacity-60 font-semibold tracking-widest">
+          {name}, {country}
+        </p>
+        <h1 className="text-8xl sm:text-9xl lg:text-9xl py-5 text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-red-700">
+          {temp}&deg;
+        </h1>
+        <p className="text-white font-semibold text-opacity-70 mb-5">
+          {new Date().toLocaleString()}
+        </p>
+        <div className="grid grid-cols-2 lg:grid-row-1 lg:grid-flow-col place-content-center gap-4 p-2">
+          {/* ------------------------------------------------------------------ */}
+          <div className="flex items-center ring ring-blue-400 ring-offset-2 ring-offset-black gap-2 bg-white p-4 rounded-lg shadow">
+            <i
+              className={
+                "wi wi-sunset text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-700 text-2xl lg:text-4xl"
+              }
+            ></i>
+            <p className="text-sm">
+              {timeStr} PM <br />
+              Sunset
+            </p>
+          </div>
+          {/* ------------------------------------------------------------------ */}
+
+          <div className="flex items-center ring ring-blue-400 ring-offset-2 ring-offset-black gap-3 bg-white p-4 rounded-lg shadow">
+            <i
+              className={
+                "wi wi-humidity text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-700 text-2xl lg:text-4xl"
+              }
+            ></i>
+            <p className="text-sm">
+              {humidity} PM <br />
+              Humidity
+            </p>
+          </div>
+          {/* ------------------------------------------------------------------ */}
+
+          <div className="flex items-center ring ring-blue-400 ring-offset-2 ring-offset-black gap-2 bg-white p-4 rounded-lg shadow">
+            <i
+              className={
+                "wi  wi-rain text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-700 text-2xl lg:text-4xl"
+              }
+            ></i>
+            <p className="text-sm">
+              {pressure} PM <br />
+              Pressure
+            </p>
+          </div>
+          {/* ------------------------------------------------------------------ */}
+
+          <div className="flex items-center ring ring-blue-400 ring-offset-2 ring-offset-black gap-2 bg-white p-2 rounded-lg shadow">
+            <i
+              className={
+                "wi wi-strong-wind text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-700 text-2xl lg:text-4xl"
+              }
+            ></i>
+            <p className="text-sm">
+              {speed} PM <br />
+              Speed
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* ================================= */}
       {/* Temperature Card */}
-      <article className="widget">
+      {/* <article className="widget">
         <div className="weatherIcon">
           <i className={`wi ${weatherState}`}></i>
         </div>
         <div className="weatherInfo">
           <div className="temperature">
-            <span>{tempInfo.temp}&deg;</span>
+            <span>{temp}&deg;</span>
           </div>
           <div className="description">
-            <div className="weatherCondition">{tempInfo.weathermood}</div>
+            <div className="weatherCondition">{weathermood}</div>
             <div className="place">
-              {tempInfo.name}, {tempInfo.country}
+              {name}, {country}
             </div>
           </div>
         </div>
         <div className="date">{new Date().toLocaleString()}</div>
 
-        {/* 4 COL SECTION */}
+        
         <div className="extra-temp">
           <div className="temp-info-minmax">
             <div className="two-sided-section">
@@ -73,7 +148,7 @@ const WeatherCard = ({ tempInfo }) => {
                 <i className={"wi wi-humidity"}></i>
               </p>
               <p className="extra-info-leftside">
-                {tempInfo.humidity} <br />
+                {humidity} <br />
                 Humidity
               </p>
             </div>
@@ -84,7 +159,7 @@ const WeatherCard = ({ tempInfo }) => {
                 <i className={"wi wi-rain"}></i>
               </p>
               <p className="extra-info-leftside">
-                {tempInfo.pressure} <br />
+                {pressure} <br />
                 Pressure
               </p>
             </div>
@@ -93,13 +168,13 @@ const WeatherCard = ({ tempInfo }) => {
                 <i className={"wi wi-strong-wind"}></i>
               </p>
               <p className="extra-info-leftside">
-                {tempInfo.speed} <br />
+                {speed} <br />
                 Speed
               </p>
             </div>
           </div>
         </div>
-      </article>
+      </article> */}
     </>
   );
 };
